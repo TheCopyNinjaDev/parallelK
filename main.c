e6f779674ac639e53cb58200c0101f952e2d2159
TheCopyNinjaDev <thecopyninjadev@gmail.com>
2024-02-25 21:20:20 +0300

    time

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
+#include <time.h> // Подключаем библиотеку time.h
+
 
 // Функция для обмена двух элементов массива
 void swap(int *a, int *b) {
@@ -42,25 +44,23 @@ int kth_order_statistic(int *a, int l, int r, int k) {
   // Определяем, в какой части находится искомая k-я порядковая статистика
   int left_size = q - l + 1;
   if (k <= left_size) {
-    // Если в левой части, то запускаем рекурсивный вызов для нее в отдельном потоке
+    // Если в левой части, то запускаем рекурсивный вызов для нее в отдельном
+    // потоке
     int result;
-    #pragma omp parallel
+#pragma omp parallel
     {
-      #pragma omp single nowait
-      {
-        result = kth_order_statistic(a, l, q, k);
-      }
+#pragma omp single nowait
+      { result = kth_order_statistic(a, l, q, k); }
     }
     return result;
   } else {
-    // Если в правой части, то запускаем рекурсивный вызов для нее в отдельном потоке
+    // Если в правой части, то запускаем рекурсивный вызов для нее в отдельном
+    // потоке
     int result;
-    #pragma omp parallel
+#pragma omp parallel
     {
-      #pragma omp single nowait
-      {
-        result = kth_order_statistic(a, q + 1, r, k - left_size);
-      }
+#pragma omp single nowait
+      { result = kth_order_statistic(a, q + 1, r, k - left_size); }
     }
     return result;
   }
@@ -77,20 +77,27 @@ void print_array(int *a, int n) {
 // Главная функция
 int main() {
   // Задаем размер и k-й элемент
-  int n = 10;
-  int k = 2;
+  int n = 1000000000;
+  int k = 5;
   // Создаем и заполняем массив случайными числами
   int *a = (int *)malloc(n * sizeof(int));
   for (int i = 0; i < n; i++) {
     a[i] = rand() % 100;
   }
   // Выводим исходный массив на экран
-  printf("Original array:\n");
-  print_array(a, n);
+  //   printf("Original array:\n");
+  //   print_array(a, n);
   // Находим k-ю порядковую статистику
+  clock_t start, end; // Объявляем переменные для измерения времени
+  start = clock();    // Запоминаем начальное время
   int kth = kth_order_statistic(a, 0, n - 1, k);
+  end = clock(); // Запоминаем конечное время
+  double time_taken = (double)(end - start) /
+                      CLOCKS_PER_SEC; // Вычисляем время выполнения в секундах
   // Выводим результат на экран
   printf("%d-th order statistic is %d\n", k, kth);
+  printf("Time taken by program is %.6f seconds\n",
+         time_taken); // Выводим время выполнения на экран
   // Освобождаем память
   free(a);
   return 0;
