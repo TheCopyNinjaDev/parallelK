08a0cba4deec8f604956795a3468c90341a38408
TheCopyNinjaDev <thecopyninjadev@gmail.com>
2024-05-15 21:43:01 +0300

    task1 done

--- main.c
+++ main.c
@@ -1,168 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "stdbool.h"
+#include "math.h"
 #include <mpi.h>
-#include <time.h>
 
-// Function to find the order statistic in an array
-int order_statistics(int *a, int n, int k) {
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Parallel code will go here
-    // For now, let's assume we're just finding the k-th order statistic
-    // without parallelization as an example
-
-    int i, j, l = 0, r = n-1, mid, swap;
-    while (1) {
-        if (r <= l + 1) {
-            if (r == l + 1 && a[r] < a[l])
-                swap = a[l], a[l] = a[r], a[r] = swap;
-            return a[k];
-        }
-
-        mid = (l + r) >> 1;
-        swap = a[mid], a[mid] = a[l + 1], a[l + 1] = swap;
-        if (a[l] > a[r])
-            swap = a[l], a[l] = a[r], a[r] = swap;
-        if (a[l + 1] > a[r])
-            swap = a[l + 1], a[l + 1] = a[r], a[r] = swap;
-        if (a[l] > a[l + 1])
-            swap = a[l], a[l] = a[l + 1], a[l + 1] = swap;
-
-        i = l + 1;
-        j = r;
-        int cur = a[l + 1];
-        while (1) {
-            while (a[++i] < cur);
-            while (a[--j] > cur);
-            if (i > j)
-                break;
-            swap = a[i], a[i] = a[j], a[j] = swap;
-        }
-
-        a[l + 1] = a[j];
-        a[j] = cur;
-
-        if (j >= k)
-            r = j - 1;
-        if (j <= k)
-            l = i;
+bool isPrime(int n) {
+    // Check for special cases
+    if (n <= 1) {
+        return false;  // Numbers <= 1 are not prime
     }
+    
+    // Check for divisors from 2 to sqrt(n)
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;  // Found a divisor other than 1 and itself
+        }
+    }
+    
+    return true;  // If no divisors other than 1 and itself, it's prime
 }
 
 int main(int argc, char *argv[]) {
-    MPI_Init(&argc, &argv);
 
     int rank, size;
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int L1 = 1;
+    double L2 = pow(10, 8);
+
+    double chunk = ceil((double)((L2-L1) / size));
+    double Nmin_loc = L1 + chunk * rank;
+    double Nmax_loc = L1 - 1 + chunk*(rank+1);
+
+    int quantity_loc = 0;
 
     double start = MPI_Wtime();
 
-    int n = 100000000; // Total size of the array
-    int k = 32935543#include <stdio.h>
-#include <stdlib.h>
-#include <mpi.h>
-#include <math.h>
-#include <complex.h>
-
-// Function to check if a point is in the Mandelbrot set
-int is_in_mandelbrot(double complex c) {
-    double complex z = 0;
-    for (int n = 0; n < 1000; n++) {
-        z = z*z + c;
-        if (cabs(z) > 2.0) return 0;
-    }
-    return 1;
-}
-
-// Main function
-int main(int argc, char *argv[]) {
-    int rank, size;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    // Monte Carlo parameters
-    int num_points = 1000000; // Number of random points
-    double area = 0.0;
-    double moment_of_inertia = 0.0;
-
-    // Seed random number generator
-    srand(rank);
-
-    // Calculate area and moment of inertia
-    for (int i = 0; i < num_points; i++) {
-        double real = (double)rand() / RAND_MAX * 4 - 2; // Real part between -2 and 2
-        double imag = (double)rand() / RAND_MAX * 4 - 2; // Imaginary part between -2 and 2
-        double complex c = real + imag * I;
-
-        if (is_in_mandelbrot(c)) {
-            area += 1.0;
-            moment_of_inertia += pow(cabs(c), 2);
+    for(int i=Nmin_loc; i<=Nmax_loc; i++)
+    {
+        if (isPrime(i)){
+            quantity_loc++;
         }
     }
 
-    // Reduce results from all processes
-    double total_area;
-    double total_moment_of_inertia;
-    MPI_Reduce(&area, &total_area, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&moment_of_inertia, &total_moment_of_inertia, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    // Master process prints the results
-    if (rank == 0) {
-        total_area *= 16.0 / (size * num_points); // Adjust for the total area of the plane
-        total_moment_of_inertia *= 16.0 / (size * num_points); // Adjust for the total number of points
-        printf("Estimated Area: %f\n", total_area);
-        printf("Estimated Moment of Inertia: %f\n", total_moment_of_inertia);
-    }
-
-    MPI_Finalize();
-    return 0;
-}
-;  // The order statistic we're looking for
-    int *a = NULL;
-    int local_n = n / size; // Size of the sub-array for each process
-
-    // Allocate memory for the sub-array on each process
-    int *local_a = malloc(local_n * sizeof(int));
-
-    // Seed the random number generator
-    srand(time(NULL) + rank);
-
-    // Each process fills its sub-array with random data
-    for (int i = 0; i < local_n; i++) {
-        local_a[i] = rand() % 100; // Random numbers between 0 and 99
-    }
-
-    // // Initialize the sub-array with the given values
-    // int values[] = {12, 1, 23, 213, 13, 2, 4, 6, 8, 9};
-    // for (int i = 0; i < local_n; i++) {
-    //     local_a[i] = values[i + rank * local_n];
-    // }
-
-    // Gather all sub-arrays into the root process
-    if (rank == 0) {
-        a = malloc(n * sizeof(int));
-    }
-    MPI_Gather(local_a, local_n, MPI_INT, a, local_n, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // The root process now has the full array and can call the order_statistics function
-    if (rank == 0) {
-        int result = order_statistics(a, n, k);
-        printf("The %d-th order statistic is %d\n", k, result);
-        free(a);
-    }
-
-    free(local_a);
     double end = MPI_Wtime();
-    MPI_Finalize();
 
+    // Gather all local sums to root (rank 0)
+    int *local_sums = NULL;
+    
     if (rank == 0) {
-        printf("\nTime measured: %f", end - start);
+        local_sums = (int *)malloc(size * sizeof(int));
+    }
+    
+    MPI_Gather(&quantity_loc, 1, MPI_INT, local_sums, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Compute global sum on root
+    if (rank == 0) {
+        int global_sum = 0;
+        for (int i = 0; i < size; i++) {
+            global_sum += local_sums[i];
+        }
+        printf("Quantity of prime nums: %d\n", global_sum);
+        free(local_sums);  // Free memory used for local_sums
+    }
+
+    MPI_Finalize();
+    if (rank == 0) {
+        printf("\nTime measured: %f\n", end - start);
     }
     return 0;
 }
-
-
